d9449a8558c5823674ea93b2947f39a95e4ef82e
cladpm@gmail.com <cladpm@gmail.com@a2e9cb7b-9436-dcf0-1598-422c8df51b5a>
2012-01-10 11:13:00 +0000

    carico file di esempio

---
+++ trunk/SF2.0 light/src/Tut05RenderedTextures.java
@@ -0,0 +1,107 @@
+
+
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+
+import shadow.image.SFBitmap;
+import shadow.image.SFFormat;
+import shadow.image.SFRenderedTexture;
+import shadow.image.SFTextureData;
+import shadow.image.SFTextureData.Filter;
+import shadow.image.SFTextureData.WrapMode;
+import shadow.pipeline.SFPipeline;
+import shadow.pipeline.SFPipelineModuleWrongException;
+import shadow.pipeline.SFProgram;
+import shadow.pipeline.loader.SFProgramComponentLoader;
+import shadow.pipeline.openGL20.SFGL20Pipeline;
+import shadow.pipeline.openGL20.tutorials.bitmapsExample.PerlinNoise3;
+import shadow.pipeline.openGL20.tutorials.utils.SFTutorial;
+
+public class Tut05RenderedTextures extends SFTutorial{
+
+	private SFTextureData texture;
+	private SFTextureData texture1;
+	private SFTextureData texture2;
+	private int shownTexture;
+	private SFProgram programGenerate;
+	private SFProgram programShow;
+	
+	public static void main(String[] args) {
+
+		SFGL20Pipeline.setup();
+
+		Tut05RenderedTextures tut03Bitmap=new Tut05RenderedTextures();
+		String[] materials={"TexturedMat"};
+		try {
+			SFProgramComponentLoader.loadComponents(new File("../ShadowFramework2.0_OpenGL20/data/pipeline/primitive"));
+
+			tut03Bitmap.programGenerate=SFPipeline.getStaticImageProgram(materials, "BasicGrayAndBright");
+			tut03Bitmap.programShow=SFPipeline.getStaticImageProgram(materials, "BasicColor");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (SFPipelineModuleWrongException e) {
+			e.printStackTrace();
+		}
+		
+		tut03Bitmap.prepareFrame("Rendered Texture", 600, 600);
+	}
+	
+	@Override
+	public void init() {
+		
+		SFBitmap bitmap=PerlinNoise3.generateBitmap();
+		
+		texture=SFPipeline.getSfTexturePipeline().getRenderedTextureFactory().generateBitmapTexture(bitmap, Filter.LINEAR,
+				WrapMode.REPEAT, WrapMode.REPEAT);
+		
+		texture1 = SFPipeline.getSfTexturePipeline().getRenderedTextureFactory().generateTextureBuffer(200, 200, SFFormat.RGB8,  Filter.LINEAR,
+				WrapMode.REPEAT, WrapMode.REPEAT);
+		texture2 = SFPipeline.getSfTexturePipeline().getRenderedTextureFactory().generateTextureBuffer(200, 200, SFFormat.RGB8,  Filter.LINEAR,
+				WrapMode.REPEAT, WrapMode.REPEAT);
+		
+		SFRenderedTexture renderedTexture=new SFRenderedTexture();
+		renderedTexture.addColorData(texture1);
+		renderedTexture.addColorData(texture2);
+		
+		SFPipeline.getSfTexturePipeline().beginNewRenderedTexture(renderedTexture);
+
+			texture.apply(0);
+		
+			SFPipeline.getSfProgramBuilder().loadProgram(programGenerate);
+	
+			SFPipeline.getSfPipelineGraphics().drawBaseQuad();
+			
+		SFPipeline.getSfTexturePipeline().endRenderedTexture(renderedTexture);
+	}
+	
+	
+	@Override
+	public void render() {
+		
+		if(shownTexture==0)
+			texture1.apply(0);
+		else if(shownTexture==1)
+			texture2.apply(0);
+		else
+			texture.apply(0);
+		
+		SFPipeline.getSfProgramBuilder().loadProgram(programShow);
+
+		SFPipeline.getSfPipelineGraphics().drawBaseQuad();
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		super.keyPressed(e);
+		if(e.getKeyCode()==KeyEvent.VK_A){
+			shownTexture=0;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_B){
+			shownTexture=1;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_C){
+			shownTexture=2;
+		}
+	}
+}
