dc041984897f6d6af31284a9198215bae2cd5394
fiii.unipv <fiii.unipv@gmail.com>
2012-02-23 08:23:53 +0000

    Shadow Framework 2.0 OpenGL2.0: alignment due to ShadowFramework 2.0 last changes, 2 new tutorials

--- ShadowFrameworkLite20_OpenGL20/src/shadow/pipeline/openGL20/SFGL20GenericProgram.java
+++ ShadowFrameworkLite20_OpenGL20/src/shadow/pipeline/openGL20/SFGL20GenericProgram.java
@@ -1,13 +1,13 @@
 package shadow.pipeline.openGL20;
 
-import shadow.pipeline.SFPipelineStructureInstance;
+import shadow.pipeline.SFPipelineStructure;
 import shadow.pipeline.SFProgram;
 import shadow.pipeline.SFStructureData;
 import shadow.system.SFInitiable;
 
 public interface SFGL20GenericProgram extends SFProgram,SFInitiable{
 
-	public abstract void setData(SFPipelineStructureInstance structure,
+	public abstract void setData(SFPipelineStructure structure,
 			SFStructureData data);
 
 }
