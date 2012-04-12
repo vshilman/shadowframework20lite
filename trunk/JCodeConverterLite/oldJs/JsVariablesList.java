d1849dc46caf042d533af40e283b6f0e483fb85f
steve3003@hotmail.it <steve3003@hotmail.it@a2e9cb7b-9436-dcf0-1598-422c8df51b5a>
2012-04-12 09:43:58 +0000

    JCodeConverterLite: moved the oldJs folder

---
+++ trunk/JCodeConverterLite/oldJs/JsVariablesList.java
@@ -0,0 +1,46 @@
+package codeconverter.js.oldJs;
+
+import codeconverter.ICodePieceSequencer;
+import codeconverter.codepieces.CodeSequence;
+import codeconverter.elements.Variable;
+import codeconverter.elements.VariableList;
+
+public class JsVariablesList {
+
+	private CodeSequence sequence=new CodeSequence(new JsName(),", ");
+	private VariableList variables=new VariableList();
+	
+	public void loadVariablesList(ICodePieceSequencer sequence) {
+		for (int i=0; i < sequence.getPieces().size(); i++) {
+			JsName name=(JsName)(sequence.getPieces().get(i).cloneCodePiece());
+			getVariables().add(new Variable(null, name.getData()));
+		}
+	}
+
+	public String toString() {
+		String tmp="";
+		
+		for (Variable va : getVariables()) {
+			tmp+=va.getName()+" ";
+		}
+		return tmp;
+	}
+	
+
+	public void setSequence(CodeSequence sequence) {
+		this.sequence = sequence;
+	}
+
+	public CodeSequence getSequence() {
+		return sequence;
+	}
+
+	public VariableList getVariables() {
+		return variables;
+	}
+
+	public void setVariables(VariableList variables) {
+		this.variables=variables;
+	}
+
+}
