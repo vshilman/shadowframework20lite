bf52b342eaf35f5ff1590dfd9a5b5e48ad735a08
swengspeople <swengspeople@gmail.com>
2013-09-20 07:57:18 +0000

    C++ added

--- JCodeComparator/src/jcodecomparator/test/TestLineStyleFactory.java
+++ JCodeComparator/src/jcodecomparator/test/TestLineStyleFactory.java
@@ -7,6 +7,8 @@ public class TestLineStyleFactory extends ConcreteLineStylerfactory{
 	public TestLineStyleFactory() {
 		map.put("java", new DefaultLineStyler(new JavaScanner()));
 		map.put("js", new DefaultLineStyler(new JsScanner()));
+		map.put("cpp", new DefaultLineStyler(new CppScanner()));
+		map.put("h", new DefaultLineStyler(new CppScanner()));
 	}
 
 }
