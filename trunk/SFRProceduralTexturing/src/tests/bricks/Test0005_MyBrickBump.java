2c4da03e065df7ecf4f276e540d1476cb515f5f7
a.provera01@gmail.com <a.provera01@gmail.com@a2e9cb7b-9436-dcf0-1598-422c8df51b5a>
2014-07-04 16:48:03 +0000

    Added Test0005_MyBrickBump Modified "basicPipeline" to support du and dv parameters Modified pipeline file "bump"

---
+++ trunk/SFRProceduralTexturing/src/tests/bricks/Test0005_MyBrickBump.java
@@ -0,0 +1,25 @@
+package tests.bricks;
+
+import shadow.pipeline.tools.SFCommonPipeline;
+import shadow.renderer.viewer.SFDrawableFrame;
+import shadow.system.SFDrawable;
+
+public class Test0005_MyBrickBump extends MainPTBricks{
+
+	private static final String FILENAME="test0005_brickbump";
+
+	public static void main(String[] args) {
+		SFCommonPipeline.setPipelineLocation("pipeline/","examplePipeline02");
+		setFilename(FILENAME);
+		execute(new Test0005_MyBrickBump());
+	}
+
+	public void viewTestData() {
+		loadLibraryAsDataCenter();
+		
+		SFDrawable drawable = getAlreadyAvailableDatasetResource("RenderedTextureViewer0002");
+		SFDrawableFrame frame = new SFDrawableFrame("Brick Wall Bump Mapping", 800, 800, drawable);
+		frame.setVisible(true);
+	}
+	
+}
