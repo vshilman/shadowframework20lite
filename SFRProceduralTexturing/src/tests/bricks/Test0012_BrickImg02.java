a0d93eb1acf76b622b2f67e990cb3140c263ee34
a.provera01 <a.provera01@gmail.com>
2014-08-12 13:45:57 +0000

    Added tests for validation phase. Modified pipeline files.

--- SFRProceduralTexturing/src/tests/bricks/Test0012_BrickImg02.java
+++ SFRProceduralTexturing/src/tests/bricks/Test0012_BrickImg02.java
@@ -4,20 +4,22 @@ import shadow.pipeline.tools.SFCommonPipeline;
 import shadow.renderer.viewer.SFDrawableFrame;
 import shadow.system.SFDrawable;
 
-public class Test0011_BrickImg01 extends MainPTBricks{
-	private static final String FILENAME="test0011_brickimg01";
+public class Test0012_BrickImg02 extends MainPTBricks{
+
+
+	private static final String FILENAME="test0012_brickimg02";
 
 	public static void main(String[] args) {
 		SFCommonPipeline.setPipelineLocation("pipeline/","examplePipeline02");
 		setFilename(FILENAME);
-		execute(new Test0011_BrickImg01());
+		execute(new Test0012_BrickImg02());
 	}
 
 	public void viewTestData() {
 		loadLibraryAsDataCenter();
 		
 		SFDrawable drawable = getAlreadyAvailableDatasetResource("RenderedTextureViewer0002");
-		SFDrawableFrame frame = new SFDrawableFrame("Brick Wall 01", 800, 800, drawable);
+		SFDrawableFrame frame = new SFDrawableFrame("Brick Wall 02", 800, 800, drawable);
 		frame.setVisible(true);
 	}
 }
--- SFRProceduralTexturing/src/tests/bricks/Test0013_BrickImg03.java
+++ SFRProceduralTexturing/src/tests/bricks/Test0013_BrickImg03.java
@@ -4,20 +4,21 @@ import shadow.pipeline.tools.SFCommonPipeline;
 import shadow.renderer.viewer.SFDrawableFrame;
 import shadow.system.SFDrawable;
 
-public class Test0011_BrickImg01 extends MainPTBricks{
-	private static final String FILENAME="test0011_brickimg01";
+public class Test0013_BrickImg03 extends MainPTBricks{
+
+	private static final String FILENAME="test0013_brickimg03";
 
 	public static void main(String[] args) {
 		SFCommonPipeline.setPipelineLocation("pipeline/","examplePipeline02");
 		setFilename(FILENAME);
-		execute(new Test0011_BrickImg01());
+		execute(new Test0013_BrickImg03());
 	}
 
 	public void viewTestData() {
 		loadLibraryAsDataCenter();
 		
 		SFDrawable drawable = getAlreadyAvailableDatasetResource("RenderedTextureViewer0002");
-		SFDrawableFrame frame = new SFDrawableFrame("Brick Wall 01", 800, 800, drawable);
+		SFDrawableFrame frame = new SFDrawableFrame("Brick Wall 03", 800, 800, drawable);
 		frame.setVisible(true);
 	}
 }
