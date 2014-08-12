d5de9ac9df0e32cda3d7587f4dd581862396d3a3
a.provera01@gmail.com <a.provera01@gmail.com@a2e9cb7b-9436-dcf0-1598-422c8df51b5a>
2014-08-12 13:45:57 +0000

    Added tests for validation phase. Modified pipeline files.

--- trunk/SFRProceduralTexturing/src/tests/bricks/Test0014_BrickImg04.java
+++ trunk/SFRProceduralTexturing/src/tests/bricks/Test0014_BrickImg04.java
@@ -4,20 +4,21 @@ import shadow.pipeline.tools.SFCommonPipeline;
 import shadow.renderer.viewer.SFDrawableFrame;
 import shadow.system.SFDrawable;
 
-public class Test0011_BrickImg01 extends MainPTBricks{
-	private static final String FILENAME="test0011_brickimg01";
+public class Test0014_BrickImg04 extends MainPTBricks{
+
+	private static final String FILENAME="test0014_brickimg04";
 
 	public static void main(String[] args) {
 		SFCommonPipeline.setPipelineLocation("pipeline/","examplePipeline02");
 		setFilename(FILENAME);
-		execute(new Test0011_BrickImg01());
+		execute(new Test0014_BrickImg04());
 	}
 
 	public void viewTestData() {
 		loadLibraryAsDataCenter();
 		
 		SFDrawable drawable = getAlreadyAvailableDatasetResource("RenderedTextureViewer0002");
-		SFDrawableFrame frame = new SFDrawableFrame("Brick Wall 01", 800, 800, drawable);
+		SFDrawableFrame frame = new SFDrawableFrame("Brick Wall 04", 800, 800, drawable);
 		frame.setVisible(true);
 	}
 }
