ba444371a2cc53972460425781115c070daca72c
a.provera01 <a.provera01@gmail.com>
2014-09-26 08:06:47 +0000

    Test values adjusted

--- SFRProceduralTexturing/src/tests/clouds/Test0024_MyCloudFractalImg02.java
+++ SFRProceduralTexturing/src/tests/clouds/Test0024_MyCloudFractalImg02.java
@@ -4,14 +4,14 @@ import shadow.pipeline.tools.SFCommonPipeline;
 import shadow.renderer.SFImageModel;
 import shadow.renderer.viewer.SFDrawableFrame;
 
-public class Test0024_MyCloudFractal extends MainPTClouds{
+public class Test0024_MyCloudFractalImg02 extends MainPTClouds{
 
-	private static final String FILENAME="test0024_mycloudfractal";
+	private static final String FILENAME="test0024_mycloudfractalImg02";
 		
 	public static void main(String[] args) {
 		SFCommonPipeline.setPipelineLocation("pipeline/","examplePipeline02");
 		setFilename(FILENAME);
-		execute(new Test0024_MyCloudFractal());
+		execute(new Test0024_MyCloudFractalImg02());
 	}
 
 	public void viewTestData() {
@@ -19,7 +19,7 @@ public class Test0024_MyCloudFractal extends MainPTClouds{
 		
 		
 		SFImageModel imageModel=getAlreadyAvailableDatasetResource("TextureViewer02");
-		SFDrawableFrame frame=new SFDrawableFrame("Fractal Cloud Test", 800, 800, imageModel);
+		SFDrawableFrame frame=new SFDrawableFrame("Fractal Cloud Test Img02", 800, 800, imageModel);
 		frame.setVisible(true);
 		
 	}
