b06a25c5a1bef5515b7931de8a0e8fc2c5619966
a.provera01 <a.provera01@gmail.com>
2014-10-29 13:51:48 +0000

    Implementation of materialComponent waterfBm in pipeline file "perlin". Modified Test_0041_MyWater. Added Test_0042WaterImg02.

--- SFRProceduralTexturing/src/tests/water/Test0042_WaterImg02.java
+++ SFRProceduralTexturing/src/tests/water/Test0042_WaterImg02.java
@@ -4,24 +4,25 @@ import shadow.pipeline.tools.SFCommonPipeline;
 import shadow.renderer.SFImageModel;
 import shadow.renderer.viewer.SFDrawableFrame;
 
-//water
-
-public class Test0041_MyWater extends MainPTWater {
+public class Test0042_WaterImg02 extends MainPTWater{
 	
-	private static final String FILENAME="test0041_mywater";
+
+		
+	private static final String FILENAME="test0042_waterimg02";
 	
 	public static void main(String[] args) {
 		SFCommonPipeline.setPipelineLocation("pipeline/","examplePipeline02");
 		setFilename(FILENAME);
-		execute(new Test0041_MyWater());
+		execute(new Test0042_WaterImg02());
 	}
 
 	public void viewTestData() {
 		loadLibraryAsDataCenter();
 		SFImageModel imageModel=getAlreadyAvailableDatasetResource("TextureViewer01");
-		SFDrawableFrame frame=new SFDrawableFrame("Water Test", 800, 800, imageModel);
+		SFDrawableFrame frame=new SFDrawableFrame("Water Test 02", 800, 800, imageModel);
 		frame.setVisible(true);
 		
 	}
 	
 }
+
