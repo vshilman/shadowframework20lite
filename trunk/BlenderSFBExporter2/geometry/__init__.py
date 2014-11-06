95bd99a66b1d662826393a846f35c3d239d455b7
flscogna@gmail.com <flscogna@gmail.com@a2e9cb7b-9436-dcf0-1598-422c8df51b5a>
2014-11-06 20:29:52 +0000

    Initial work on testing.

--- trunk/BlenderSFBExporter2/geometry/__init__.py
+++ trunk/BlenderSFBExporter2/geometry/__init__.py
@@ -1,13 +1,10 @@
-from base import Vertex
+from .base import Vertex
 
-from curves import BaseCurve
-from curves import SuperCurve 
-from curves import SubCurve
+from .curves import BaseCurve
+from .curves import SuperCurve 
+from .curves import SubCurve
 
-from samplers import sample_curve_samples
-from samplers import sample_curve_step
-from samplers import sample_patch_samples
-from samplers import sample_patch_step
-
-from errors import simple_sq_error
-from errors import verts_sets_sq_error
+from .samplers import sample_curve_samples
+from .samplers import sample_curve_step
+from .samplers import sample_patch_samples
+from .samplers import sample_patch_step
--- trunk/BlenderSFBExporter2/mymath/__init__.py
+++ trunk/BlenderSFBExporter2/mymath/__init__.py
@@ -1,14 +1,14 @@
-from bernstein import bernstein_poly
-from bernstein import bernstein_poly_2D
-from bernstein import bezier_curve_n
+from .bernstein import bernstein_poly
+from .bernstein import bernstein_poly_2D
+from .bernstein import bezier_curve_n
 
-from bezier_curves import bezier_curve_1
-from bezier_curves import bezier_curve_2
-from bezier_curves import bezier_curve_3
+from .bezier_curves import bezier_curve_1
+from .bezier_curves import bezier_curve_2
+from .bezier_curves import bezier_curve_3
 
-from bezier_curves import compile_bezier_curve
-from bezier_curves import compile_bezier_curve_1
-from bezier_curves import compile_bezier_curve_2
-from bezier_curves import compile_bezier_curve_3
+from .bezier_curves import compile_bezier_curve
+from .bezier_curves import compile_bezier_curve_1
+from .bezier_curves import compile_bezier_curve_2
+from .bezier_curves import compile_bezier_curve_3
 
-from bezier_patches import *
+from .bezier_patches import *
