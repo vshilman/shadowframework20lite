1e2a1d5c8f9fe94faa982956f14c59d9da9db047
andrea.pasquali01 <andrea.pasquali01@gmail.com>
2015-03-06 18:44:23 +0000

    Refactoring totale in preparazione Alpha

--- CloudComputingServerSide/src/utils/Incapsulator.java
+++ CloudComputingServerSide/src/utils/Incapsulator.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Set;
 
 
-public class Incapsulator {
+public class Incapsulator{
 
 	private static String HEADERLIST="<listaOnline>";
 	private static String HEADERUSER="<user>";
@@ -22,30 +22,56 @@ public class Incapsulator {
 	private static String FOOTERIP="</ip>";
 	private static String FOOTERGAME="</game>";
 	private static String FILEHEADER="<?xml version=\"1.0\"?>";
-	private Set<String> nicks;
+	private static String FILEMESSAGEHEADER="<messageList>";
+	private static String MESSAGEHEADER="<message>";
+	private static String FILEMESSAGEFOOTER="</messageList>";
+	private static String MESSAGEFOOTER="</message>";
+	private Set<String> mainTag;
 	private List<String> message= new ArrayList<String>();
-	private String message1;
+	private String messageConverted;
 	
-	public String convert(HashMap<String, List<String>> mapToConvert) {
+	
+	
+	
+	
+	public String convertUserMap(HashMap<String, List<String>> mapToConvert) {
 		
-		nicks=mapToConvert.keySet();
-		message.add(FILEHEADER);
-		message.add(HEADERLIST);
-		message1=FILEHEADER+HEADERLIST;
-		for (Iterator<String> iterator = nicks.iterator(); iterator.hasNext();) {
+		mainTag=mapToConvert.keySet();
+		messageConverted=FILEHEADER+HEADERLIST;
+		for (Iterator<String> iterator = mainTag.iterator(); iterator.hasNext();) {
 			String nick = (String) iterator.next();
-			message.add(HEADERUSER);
-			message.add(HEADERNICK+nick+FOOTERNICK);
-			message.add(HEADERIP+mapToConvert.get(nick).get(0)+FOOTERIP);
-			message.add(HEADERPLATFORM+mapToConvert.get(nick).get(1)+FOOTERPLATFORM);
-			message.add(HEADERGAME+mapToConvert.get(nick).get(2)+FOOTERGAME);
-			message.add(FOOTERUSER);
-			message1=message1+HEADERUSER+HEADERNICK+nick+FOOTERNICK+HEADERIP+mapToConvert.get(nick).get(0)+FOOTERIP+HEADERPLATFORM+mapToConvert.get(nick).get(1)+FOOTERPLATFORM+HEADERGAME+mapToConvert.get(nick).get(2)+FOOTERGAME+FOOTERUSER;
+			messageConverted=messageConverted+HEADERUSER+HEADERNICK+nick+FOOTERNICK+HEADERIP+mapToConvert.get(nick).get(0)+FOOTERIP+HEADERPLATFORM+mapToConvert.get(nick).get(1)+FOOTERPLATFORM+HEADERGAME+mapToConvert.get(nick).get(2)+FOOTERGAME+FOOTERUSER;
 		}
-		message1=message1+FOOTERLIST;
-		message.add(FOOTERLIST);
+		messageConverted=messageConverted+FOOTERLIST;
 		
-		return message1;
+		return messageConverted;
 	}
 	
+	
+	public String convertMessageList(List<String> messages){
+		messageConverted=FILEHEADER+FILEMESSAGEHEADER;
+		for (int i = 0; i < messages.size(); i++) {
+			String actualMessage = messages.get(i);
+			messageConverted=messageConverted+MESSAGEHEADER+actualMessage+MESSAGEFOOTER;
+		}
+		messageConverted=messageConverted+FILEMESSAGEFOOTER;
+		
+		return messageConverted;
+	}
+	
+	public String convertWelcomer(List<String> welcomeUser){
+		messageConverted=FILEHEADER+HEADERLIST;
+		messageConverted=messageConverted+HEADERUSER+HEADERNICK+welcomeUser.get(0)+FOOTERNICK+HEADERIP+welcomeUser.get(1)+FOOTERIP+HEADERPLATFORM+welcomeUser.get(2)+FOOTERPLATFORM+HEADERGAME+welcomeUser.get(3)+FOOTERGAME+FOOTERUSER;
+		messageConverted=messageConverted+FOOTERLIST;
+		
+		return messageConverted;
+	}
+	
+	public String convert(List<String> listToConvert, String headerList, List<String> headers ){
+		return null;
+	}
+	
+	public String convert(List<String> listToConvert, String headerList, String elementsHeader, List<String> headers ){
+		return null;
+	}
 }
